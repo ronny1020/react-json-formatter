e2a2a406759eab3757c311a4b994f440ea2c2ac2
Ronny Hsieh <1020ronny@gmail.com>
2020-06-02 20:29:31 +0800

    init

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,42 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-json-formater'
+import { JsonFormatter } from 'react-json-formater'
 import 'react-json-formater/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const sample = `{
+  "name": "react-json-formater-example",
+  "homepage": ".",
+  "version": "0.0.0",
+  "private": true,
+  "scripts": {
+    "start": "node ../node_modules/react-scripts/bin/react-scripts.js start",
+    "build": "node ../node_modules/react-scripts/bin/react-scripts.js build",
+    "test": "node ../node_modules/react-scripts/bin/react-scripts.js test",
+    "eject": "node ../node_modules/react-scripts/bin/react-scripts.js eject"
+  },
+  "dependencies": {
+    "react": "file:../node_modules/react",
+    "react-dom": "file:../node_modules/react-dom",
+    "react-scripts": "file:../node_modules/react-scripts",
+    "react-json-formater": "file:.."
+  },
+  "devDependencies": {
+    "@babel/plugin-syntax-object-rest-spread": "^7.8.3"
+  },
+  "eslintConfig": {
+    "extends": "react-app"
+  },
+  "browserslist": [
+    ">0.2%",
+    "not dead",
+    "not ie <= 11",
+    "not op_mini all"
+  ]
+}
+`
+
+  return <JsonFormatter json={sample} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const JsonFormatter = ({ json }) => {
+  const jsonObject = JSON.parse(json)
+
+  return (
+    <React.Fragment>
+      <span>{'{'}</span>
+      <div>{json}</div>
+      <span>{'}'}</span>
+    </React.Fragment>
+  )
 }
