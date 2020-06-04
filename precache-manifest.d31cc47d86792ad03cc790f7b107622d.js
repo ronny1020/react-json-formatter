8649890b3b37bcfd8b9bd4ef042eea9f2787015c
Ronny Hsieh <1020ronny@gmail.com>
2020-06-05 00:29:39 +0800

    Updates

---
+++ precache-manifest.d31cc47d86792ad03cc790f7b107622d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642fed76e4a1d0a9f73345d4093dd404",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a54cf18c923b2dfeea8",
+    "url": "./static/css/main.8a4eb0fc.chunk.css"
+  },
+  {
+    "revision": "96f3bdb155b0b692d6cc",
+    "url": "./static/js/2.49d99c62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.49d99c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a54cf18c923b2dfeea8",
+    "url": "./static/js/main.2950cc55.chunk.js"
+  },
+  {
+    "revision": "0ca8c36b4e42488a8514",
+    "url": "./static/js/runtime-main.4ecb783e.js"
+  }
+]);
