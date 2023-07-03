c67629dac0fa6fda50fcc70a0d9f6e51d93a28ea
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2023-07-03 21:09:51 +0800

    test(vitest): setup coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      reporter: ['text', 'json-summary', 'json'],
+      statements: 60
+    },
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./tests/setup.ts']
